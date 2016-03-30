1d821f2acfcd755990ee64369a59bb5b2e9a8094
Arki Valencia II <arvalencia@gobx.adnu.edu.ph>
2016-03-30 11:39:57 -0400

    updated base sql code

--- tournament.sql
+++ tournament.sql
@@ -6,29 +6,30 @@ CREATE DATABASE tournament;
 \c tournament;
 
 CREATE TABLE matches (
-	match_id integer PRIMARY KEY,
+	match_id SERIAL PRIMARY KEY,
 	player_left integer,
 	player_right integer,
 	player_winner integer
 );
 
-INSERT INTO matches VALUES(1, 1, 2, 1);
-INSERT INTO matches VALUES(2, 1, 4, 1);
-INSERT INTO matches VALUES(3, 1, 3, 3);
-INSERT INTO matches VALUES(4, 1, 5, 1);
-INSERT INTO matches VALUES(5, 1, 6, 6);
-INSERT INTO matches VALUES(6, 1, 7, 1);
+INSERT INTO matches ("player_left", "player_right", "player_winner") VALUES(1, 2, 1);
+INSERT INTO matches ("player_left", "player_right", "player_winner") VALUES(1, 4, 1);
+INSERT INTO matches ("player_left", "player_right", "player_winner") VALUES(1, 3, 3);
+INSERT INTO matches ("player_left", "player_right", "player_winner") VALUES(1, 5, 1);
+INSERT INTO matches ("player_left", "player_right", "player_winner") VALUES(1, 6, 6);
+INSERT INTO matches ("player_left", "player_right", "player_winner") VALUES(1, 7, 1);
 
 CREATE TABLE players (
-	player_id integer PRIMARY KEY,
-	player_name string,
+	player_id SERIAL PRIMARY KEY,
+	player_name varchar,
 	player_num_wins integer,
+	player_played_matches integer
 );
 
-INSERT INTO players VALUES(1, "onin1", 0);
-INSERT INTO players VALUES(2, "onin2", 0);
-INSERT INTO players VALUES(3, "onin3", 0);
-INSERT INTO players VALUES(4, "onin4", 0);
-INSERT INTO players VALUES(5, "onin5", 0);
-INSERT INTO players VALUES(6, "onin6", 0);
-INSERT INTO players VALUES(7, "onin7", 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin1', 0, 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin2', 0, 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin3', 0, 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin4', 0, 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin5', 0, 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin6', 0, 0);
+INSERT INTO players ("player_name", "player_num_wins", "player_played_matches") VALUES('onin7', 0, 0);
